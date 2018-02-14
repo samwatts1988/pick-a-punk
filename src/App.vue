69f20653a0afc62c6e7ebcfc5f31eafcb41fbda8
S B Watts <sbwatts@Ss-MacBook-Pro.local>
2018-02-14 15:47:43 +0000

    Routing

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'brews' }">Brews</router-link> |
+      <router-link to="/brews/1">Brew 1</router-link>
     </div>
     <router-view/>
   </div>
